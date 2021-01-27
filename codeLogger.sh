2ea237df373aa4d0d583dccbcecbc31130dde313
lmgibson <landon.g42@gmail.com>
2021-01-27 13:38:37 -0800

    Added the ability to find and source do files. Fixed a bug where the script would ignore code direclty within the code directory. For example, if your code is in ./code/script.py it wouldn't see it. Also fixed a typo where the sed for python was swapping out .R and not .py (copy paste mishap).

--- codeLogger.sh
+++ codeLogger.sh
@@ -1,33 +1,44 @@
 #!/usr/bin/bash
 
 # On development branch
-# Mirroring the directory structure of the folder containing code
+
+# Creating list of files to log
 cd $1
+find $i -type f \( -iname \*.R -o -iname \*.py -o -iname \*.do \) >> ../filesToLog.txt
+
+# Mirroring the directory structure of the folder containing code
 for i in */
 do
     if [[ $i != *"archive"* ]]; then
-        find $i -type d -exec mkdir -p ../logs_test/{} \;
-        find $i -type f \( -iname \*.R -o -iname \*.py \) >> ../logs_test/filesToLog.txt
+        find $i -type d -exec mkdir -p ../logs/{} \;
     fi
 done
 cd ../
+mv ./filesToLog.txt ./logs/
 
 # Sourcing files
 echo -e "\n\033[32mSourcing Files ...\033[39m\n"
-cat ./logs_test/filesToLog.txt | while read i; do
+cat ./logs/filesToLog.txt | while read i; do
     if [[ $i == *".R" ]]; then
         today=$(date +"%Y-%m-%d-%H%M")
-        fileName=$(echo ".logs_test/$i" | sed "s/\.R/_$today\.txt/")
+        fileName=$(echo "./logs/$i" | sed "s/\.R/_$today\.txt/")
         Rscript $1/$i >& $fileName
         echo "Logged $i"
     elif [[ $i == *".py" ]]; then
         today=$(date +"%Y-%m-%d-%H%M")
-        fileName=$(echo ".logs_test/$i" | sed "s/\.R/_$today\.txt/")
+        fileName=$(echo "./logs/$i" | sed "s/\.py/_$today\.txt/")
         python.exe $1/$i >& $fileName
+        echo "Logged $i"
+    elif [[ $i == *".do" ]]; then
+        today=$(date +"%Y-%m-%d-%H%M")
+        fileName=$(echo "./logs/$i" | sed "s/\.do/_$today\.txt/")
+        stata -e do $1/$i
+        cat $i.log >& $fileName
+        rm *.log
         echo "Logged $i"
     fi
 done
 
 # Cleaning up
 echo -e "\n\033[32mFinished Logging\033[39m"
-rm ./logs_test/filesToLog.txt
+rm ./logs/filesToLog.txt
